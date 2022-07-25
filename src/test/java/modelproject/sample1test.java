932874cbb95e7eed82afc31edcd6142f2586596b
Tejaswini-10 <103121648+Tejaswini-10@users.noreply.github.com>
2022-07-25 14:49:06 +0530

    Update sample1test.java

--- src/test/java/modelproject/sample1test.java
+++ src/test/java/modelproject/sample1test.java
@@ -5,6 +5,9 @@ public class sample1test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First git project");
+		System.out.println("First git project");
+		System.out.println("First git project");
+		
 	}
 
 }
